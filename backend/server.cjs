637a2737c77015ef04ce46834d9f755beff5a1e5
usjitendra <usjitendra055@gmail.com>
2025-12-21 15:41:04 +0530

    update

--- backend/server.cjs
+++ backend/server.cjs
@@ -210,7 +210,7 @@ app.use('/uploads', express.static(path.join(__dirname, 'Uploads')));
 app.get('/', (req, res) => {
   const dbStatus = mongoose.connection.readyState;
   res.json({
-    status: 'Server is running CI CD kare chal',
+    status: 'Server is running CI CD kare chal111',
     dbStatus: dbStatus === 1 ? 'Connected' : 'Disconnected',
     environment: NODE_ENV
   });
